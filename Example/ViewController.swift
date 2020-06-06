50fd98643467cf14663bef4db1851dce4a56655f
Roman Misnikov <rmisnikov@yotateam.com>
2020-06-06 19:19:54 +0300

    Add 3 simple screens

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Роман Мисников on 30.05.2020.
+//  Copyright © 2020 Роман Мисников. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
